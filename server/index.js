ffd8117cba2b84aa4874bedef86398a656ffc68d
Guilherme Silva <guilhermeduarte19@gmail.com>
2022-12-24 10:24:48 -0300

    alteração

--- server/index.js
+++ server/index.js
@@ -2,11 +2,15 @@ const express = require("express");
 
 const PORT = process.env.PORT || 3001;
 
+const cors = require("cors");
+
 const app = express();
+
+app.use(cors());
 
 const FunctionController = require("./Function/FunctionController");
 
-app.get("/api", FunctionController.getMultiplier);
+app.get("/", FunctionController.getMultiplier);
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
